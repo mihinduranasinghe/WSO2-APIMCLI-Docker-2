2463de0f9824b5a6a4dd9d932cf5a60d876b8528
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-27 23:34:45 +0530

    Client access token generate automation

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -85,10 +85,11 @@ echo "::end-group"
 echo "::group::Client Registration"
 # curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
 # curl -X POST -H "Authorization: Basic base64encode($1:$2)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
-base64key=`echo -n "$1:$2" | base64`
+# base64key1 = Authorization: Basic  username@wso2.com@organizationname:password
+base64key1=`echo -n "$1:$2" | base64`
 
 response_clientId=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/client-registration/register' \
---header "Authorization: Basic $base64key" \
+--header "Authorization: Basic $base64key1" \
 --header "Content-Type: application/json" \
 --data-raw '{
     "callbackUrl": "www.google.lk",
@@ -97,7 +98,8 @@ response_clientId=`curl -s --location -g --request POST 'https://gateway.api.clo
     "owner": "'$1'",
     "grantType": "password refresh_token",
     "saasApp": true
-}' | jq --raw-output '.clientId'`
+}' | jq '.'`
+
 
 echo $response_clientId
 echo "::end-group"
@@ -105,7 +107,7 @@ echo "::end-group"
 
 echo "::group::Client Access Token Generate"
 # curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
-# Authorization: Basic  username@wso2.com@organizationname:password
+# base64key2 = Authorization: Basic rest-client-id:rest-client-secret
 
 response_client_access_token_generate=`curl -s --location -g --request POST --verbose 'https://gateway.api.cloud.wso2.com/token' \
 --header "Content-Type: application/x-www-form-urlencoded" \
