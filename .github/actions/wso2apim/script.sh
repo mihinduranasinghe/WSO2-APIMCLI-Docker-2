99087dfd5308a4090414c10bb3dc8328ff62e382
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-27 19:36:15 +0530

    Client access token generation automation

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -100,6 +100,7 @@ response_client_registration=`curl -i --location -g --request POST --verbose 'ht
 }'`
 
 echo $response_client_registration
+echo `$response_client_registration["clientId"]
 echo "::end-group"
 
 
