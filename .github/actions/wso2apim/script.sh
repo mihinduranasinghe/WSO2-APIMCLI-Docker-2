20a7e7bb68efe990610d198e5d8d15aadb4a44cc
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-29 00:14:41 +0530

    Main Script backup before refactoring

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -160,7 +160,7 @@ API_response=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso
 all_APIs_list=`echo "$API_response" | jq '.list'`
 relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$3'" and .version=="'$4'")'`
 api_identifier=`echo "$relevant_api" | jq --raw-output '.id'`
-# echo $APIs
+echo $APIs
 # echo $all_APIs_list
 # echo $relevant_api
 echo $api_identifier
