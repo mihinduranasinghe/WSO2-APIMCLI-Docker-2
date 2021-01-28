b623223a070b950b2dbcddafc0eeb3c83633002a
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-29 02:16:52 +0530

    debugging

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -131,6 +131,10 @@ echo "::group::Finding The API Identifier(apiId)"
     relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$3'" and .version=="'$4'")'`
     api_identifier=`echo "$relevant_api" | jq --raw-output '.id'`
     
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
     # echo $GET_APIs_response
     # echo $all_APIs_list
     # echo $relevant_api
