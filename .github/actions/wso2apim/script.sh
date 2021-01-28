a1e825431bf36e830995597951d85494183ac95e
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-29 03:31:08 +0530

    Code refactoring

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -10,14 +10,14 @@
         #                                         |
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#Re-assigning user inputs into variables
+## Re-assigning user inputs into variables
     username=`echo "$1"`
     password=`echo "$2"`
     APIName=`echo "$3"`
     APIVersion=`echo "$4"`
     PostmanCollectionTestFile=`echo "$5"`
 
-# Echo user inputs
+## Echo user inputs
 echo "::group::WSO2 APIMCloud - Your Inputs"
     echo Username Targeted tenant  - $username
     echo Password                  - $password
@@ -26,7 +26,7 @@ echo "::group::WSO2 APIMCloud - Your Inputs"
     echo PostmanCollectionTestFile - $PostmanCollectionTestFile 
 echo "::end-group"
 
-# Confiduring WSO2 API Cloud gateway environment in VM
+## Confiduring WSO2 API Cloud gateway environment in VM
 echo "::group::Add environment wso2apicloud"
     apimcli add-env -n wso2apicloud \
                         --registration https://gateway.api.cloud.wso2.com/client-registration/register \
@@ -41,7 +41,7 @@ echo "::group::Add environment wso2apicloud"
 echo "::end-group"
 
 
-# Init API iproject with given API definition
+## Init API iproject with given API definition
 echo "::group::Init API iproject with given API definition"
     apimcli init ./$APIName/$APIVersion 
     mkdir ./$APIName/$APIVersion/Sequences/fault-sequence/Custom
@@ -53,6 +53,7 @@ echo "::group::Init API iproject with given API definition"
 echo "::end-group"
 
 
+## Push newly initialized API project into the GIT repo again from VM
 echo "::group::Push API project into the GIT repo from VM"
     git config --global user.email "my-bot@bot.com"
     git config --global user.name "my-bot"
@@ -66,37 +67,30 @@ echo "::group::Push API project into the GIT repo from VM"
 echo "::end-group"
 
 
+## Import/deploy API project to the targetted Tenant
 echo "::group::Import API project to targetted Tenant"
     apimcli login wso2apicloud -u $username -p $password -k
     apimcli import-api -f ./$APIName/$APIVersion -e wso2apicloud --preserve-provider=false --update --verbose -k
 echo "::end-group"
+                                                 
 
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-    echo "test"
-
-echo "::group::List APIS in targetted Tenant"
+## Listing the APIS in targeted Tenant
+echo "::group::List APIS in targeted Tenant"
     # apimcli list apis -e <environment> -k
     # apimcli list apis --environment <environment> --insec
     apimcli list apis -e wso2apicloud -k
 echo "::end-group"
 
 
-#-----------------------------------------------------------------Invoking an API Access Token
+################~~ Invoking an API Access Token ~~################
 
+## Register a WSO2 Cloud REST API client
 echo "::group::REST Client Registration"
-    # curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
+    
     # base64key1 = Authorization: Basic  <username@wso2.com@organizationname:password>base64
     base64key1=`echo -n "$username:$password" | base64`
 
+    # curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
     rest_client_object=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/client-registration/register' \
     --header "Authorization: Basic $base64key1" \
     --header "Content-Type: application/json" \
@@ -117,12 +111,14 @@ echo "::group::REST Client Registration"
 echo "::end-group"
 
 
+## Generate access tokens for the REST Client for different scopes (api_view, subscribe, subscription_view, ...)
 echo "::group::REST Client Access Token Generate"
-    # curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
+    
     # base64key2 = Authorization: Basic <rest-client-id:rest-client-secret>base64
     base64key2=`echo -n "$rest_clientId:$rest_clientSecret" | base64`
 
-    rest_access_token_scope_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
+    # curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
+    rest_access_token_api_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --header "Authorization: Basic $base64key2" \
     --data-urlencode "grant_type=password" \
@@ -146,15 +142,17 @@ echo "::group::REST Client Access Token Generate"
     --data-urlencode "password=$password" \
     --data-urlencode "scope=apim:subscription_view" | jq --raw-output '.access_token'`
 
-    # echo $rest_access_token_scope_view
+    # echo $rest_access_token_api_view
     # echo $rest_access_token_subscribe
     # echo $rest_access_token_subscription_view
 echo "::end-group"
 
 
+## Finding The API Identifier(apiId) of the user's respective API
 echo "::group::Finding The API Identifier(apiId)"
+
     GET_APIs_response=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/publisher/apis' \
-    --header "Authorization: Bearer $rest_access_token_scope_view"`
+    --header "Authorization: Bearer $rest_access_token_api_view"`
 
     all_APIs_list=`echo "$GET_APIs_response" | jq '.list'`
     relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$APIName'" and .version=="'$APIVersion'")'`
@@ -200,18 +198,30 @@ echo "::group::Finding The API Identifier(apiId)"
     echo "test"
     echo "test"
     echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
+    echo "test"
     # echo $GET_APIs_response
     # echo $all_APIs_list
     # echo $relevant_api
-    echo $api_identifier
+    echo API Identifier - $api_identifier
+
 echo "::end-group"
 
-
+## Create A New Application named "TestingAutomationApp" for testing purpose
 echo "::group::Create A New Application - TestingAutomationApp"
-    # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json "https://gateway.api.cloud.wso2.com/api/am/store/applications"
 
     view_applications_response=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/store/applications' \
     --header "Authorization: Bearer $rest_access_token_subscribe"`
+    # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json "https://gateway.api.cloud.wso2.com/api/am/store/applications"
 
     applications_list=`echo "$view_applications_response" | jq '.list'`
     testing_automation_application=`echo "$applications_list" | jq '.[] | select(.name=="TestingAutomationApp")'`
@@ -220,7 +230,7 @@ echo "::group::Create A New Application - TestingAutomationApp"
     # echo $view_applications_response
     # echo $applications_list
     # echo $testing_automation_application
-    echo $application_id
+    echo ApplicationID - $application_id
 
     if [ -z "$application_id" ]
         then
@@ -235,12 +245,13 @@ echo "::group::Create A New Application - TestingAutomationApp"
         }'`
 
         application_id=`echo "$new_testing_automation_application" | jq --raw-output '.applicationId'`
-        echo $application_id
+        echo ApplicationID - $application_id
     fi
 echo "::end-group"
 
-
+## Add a new subscription from newly created "TestingAutomationApp" to the current API
 echo "::group::Add a new subscription"
+
     # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST  -d @data.json "https://gateway.api.cloud.wso2.com/api/am/store/subscriptions"
     view_api_subscriptions_response=`curl -s --location -g --request GET "https://gateway.api.cloud.wso2.com/api/am/publisher/subscriptions?apiId=$api_identifier" \
     --header "Authorization: Bearer $rest_access_token_subscription_view"`
@@ -248,10 +259,12 @@ echo "::group::Add a new subscription"
     api_subscriptions_list=`echo "$view_api_subscriptions_response" | jq '.list'`
     testing_automation_app_subscription=`echo "$api_subscriptions_list" | jq '.[] | select(.applicationId=="'$application_id'")'`
     subscription_id=`echo "$testing_automation_app_subscription" | jq --raw-output '.subscriptionId'`
+    
     # echo $view_api_subscriptions_response
     # echo $api_subscriptions_list
     # echo $testing_automation_app_subscription
-    echo $subscription_id
+    # echo $subscription_id
+    
     if [ -z "$subscription_id" ]
         then
         add_subscription_response=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/api/am/store/subscriptions' \
@@ -266,16 +279,17 @@ echo "::group::Add a new subscription"
         echo $add_subscription_response
     fi 
 
-    echo $subscription_id
-    echo $application_id
+    # echo $subscription_id
+    # echo $application_id
 echo "::end-group"
 
 
-echo "::group::Generate consumer Keys(client keys) and secrets for for the Testing Automation Application"
-    # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json  "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=c30f3a6e-ffa4-4ae7-afce-224d1f820524"
+## Generate consumer Keys(client key and secret) for for the Testing Automation Application
+echo "::group::Generate consumer Keys(client key and secret) for for the Testing Automation Application"
+
     view_application_access_keys_response=`curl -s --location -g --request GET "https://gateway.api.cloud.wso2.com/api/am/store/applications/$application_id/keys/PRODUCTION" \
     --header "Authorization: Bearer $rest_access_token_subscribe"`
-    # echo $view_application_access_keys_response
+    # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json  "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=c30f3a6e-ffa4-4ae7-afce-224d1f820524"
 
     if [ "$view_application_access_keys_response" ]
         then
@@ -297,16 +311,18 @@ echo "::group::Generate consumer Keys(client keys) and secrets for for the Testi
         consumer_secret=`echo "$application_access_response" | jq --raw-output '.consumerSecret'`
     fi 
 
-        echo $consumer_key
-        echo $consumer_secret
+        echo consumer_key    - $consumer_key
+        echo consumer_secret - $consumer_secret
 echo "::end-group"
 
 
+## Generate access token for your API
 echo "::group::Generate access token for your API"
-    # curl -u <client id>:<client secret> -k -d "grant_type=client_credentials&validity_period=3600" -H "Content-Type:application/x-www-form-urlencoded" https://gateway.api.cloud.wso2.com:443/token
+    
     # base64 encode<consumer-key:consumer-secret>
     base64key3=`echo -n "$consumer_key:$consumer_secret" | base64`
 
+    # curl -u <client id>:<client secret> -k -d "grant_type=client_credentials&validity_period=3600" -H "Content-Type:application/x-www-form-urlencoded" https://gateway.api.cloud.wso2.com:443/token
     api_access_response=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com:443/token' \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --header "Authorization: Basic $base64key3" \
@@ -316,7 +332,8 @@ echo "::group::Generate access token for your API"
     api_access_token=`echo "$api_access_response" | jq --raw-output '.access_token'`
    
     # echo $api_access_response
-    echo $api_access_token
+    echo API ACCESS TOKEN - $api_access_token
+
 echo "::end-group"
 
 
