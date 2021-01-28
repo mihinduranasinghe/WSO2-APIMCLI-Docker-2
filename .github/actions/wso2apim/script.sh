b7bc673d188122dee2799808d8aa28e0093149db
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-28 15:40:14 +0530

    jq filtering

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -85,7 +85,6 @@ echo "::end-group"
 
 echo "::group::Client Registration"
 # curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
-# curl -X POST -H "Authorization: Basic base64encode($1:$2)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
 # base64key1 = Authorization: Basic  <username@wso2.com@organizationname:password>base64
 base64key1=`echo -n "$1:$2" | base64`
 
@@ -104,27 +103,15 @@ rest_client_object=`curl -s --location -g --request POST 'https://gateway.api.cl
 rest_clientId=`echo "$rest_client_object" | jq --raw-output '.clientId'`
 rest_clientSecret=`echo "$rest_client_object" | jq --raw-output '.clientSecret'`
 
-# rest_clientSecret=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/client-registration/register' \
-# --header "Authorization: Basic $base64key1" \
-# --header "Content-Type: application/json" \
-# --data-raw '{
-#     "callbackUrl": "www.google.lk",
-#     "clientName": "rest_api_store",
-#     "tokenScope": "Production",
-#     "owner": "'$1'",
-#     "grantType": "password refresh_token",
-#     "saasApp": true
-# }' | jq --raw-output '.clientSecret'`
-
 echo $rest_clientId
 echo $rest_clientSecret 
-base64key2=`echo -n "$rest_clientId:$rest_clientSecret" | base64`
 echo "::end-group"
 
 
 echo "::group::Client Access Token Generate"
 # curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
 # base64key2 = Authorization: Basic <rest-client-id:rest-client-secret>base64
+base64key2=`echo -n "$rest_clientId:$rest_clientSecret" | base64`
 
 rest_access_token_scope_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
 --header "Content-Type: application/x-www-form-urlencoded" \
@@ -149,11 +136,10 @@ echo "::end-group"
 
 
 echo "::group::Finding the API identifier"
-
-API_Identifier=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/publisher/apis' \
+All_APIs=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/publisher/apis' \
 --header "Authorization: Bearer $rest_access_token_scope_view"`
 
-echo $API_Identifier
+echo $All_APIs
 echo "::end-group"
 
 
@@ -164,8 +150,7 @@ echo "::end-group"
 
 echo "::group::Create new Application"
 # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json "https://gateway.api.cloud.wso2.com/api/am/store/applications"
-
-application_id=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/api/am/store/applications' \
+new_test_automation_application=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/api/am/store/applications' \
 --header "Authorization: Bearer $rest_access_token_subscribe" \
 --header "Content-Type: application/json" \
 --data-raw '{
@@ -173,7 +158,9 @@ application_id=`curl -s --location -g --request POST 'https://gateway.api.cloud.
     "description": "Automatic generated app for automated testing purpose",
     "name": "TestAutomationApp",
     "callbackUrl": "http://my.server.com/callback"
-}' | jq --raw-output '.applicationId'`
+}'`
+
+application_id=`echo "$new_test_automation_application" | jq --raw-output '.applicationId'`
 
 echo $application_id
 echo "::end-group"
