32127fbf9a1a61edf52108b132f13eeac5394111
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-29 01:07:40 +0530

    debugging

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -152,7 +152,7 @@ echo "::group::Finding the API identifier"
     relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$3'" and .version=="'$4'")'`
     api_identifier=`echo "$relevant_api" | jq --raw-output '.id'`
     
-    # echo $API_response
+    echo $API_response
     # echo $all_APIs_list
     # echo $relevant_api
     echo $api_identifier
@@ -215,60 +215,62 @@ echo "::group::Add a new subscription"
             "apiIdentifier": "'$api_identifier'",
             "applicationId": "'$application_id'"
         }'`
+
         echo $add_subscription_response
     fi 
+
     echo $subscription_id
     echo $application_id
 echo "::end-group"
 
 
-
 echo "::group::Generate consumer Keys(client keys) and secrets for for the Testing Automation Application"
-# curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json  "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=c30f3a6e-ffa4-4ae7-afce-224d1f820524"
-view_application_access_keys_response=`curl -s --location -g --request GET "https://gateway.api.cloud.wso2.com/api/am/store/applications/$application_id/keys/PRODUCTION" \
---header "Authorization: Bearer $rest_access_token_subscribe"`
+    # curl -k -H "Authorization: Bearer ae4eae22-3f65-387b-a171-d37eaa366fa8" -H "Content-Type: application/json" -X POST -d @data.json  "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=c30f3a6e-ffa4-4ae7-afce-224d1f820524"
+    view_application_access_keys_response=`curl -s --location -g --request GET "https://gateway.api.cloud.wso2.com/api/am/store/applications/$application_id/keys/PRODUCTION" \
+    --header "Authorization: Bearer $rest_access_token_subscribe"`
+    # echo $view_application_access_keys_response
 
-# echo $view_application_access_keys_response
+    if [ "$view_application_access_keys_response" ]
+        then
+        consumer_key=`echo "$view_application_access_keys_response" | jq --raw-output '.consumerKey'`
+        consumer_secret=`echo "$view_application_access_keys_response" | jq --raw-output '.consumerSecret'`
 
-if [ "$view_application_access_keys_response" ]
-    then
-    consumer_key=`echo "$view_application_access_keys_response" | jq --raw-output '.consumerKey'`
-    consumer_secret=`echo "$view_application_access_keys_response" | jq --raw-output '.consumerSecret'`
+        else
+        application_access_response=`curl -s --location -g --request POST "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=$application_id" \
+        --header "Authorization: Bearer $rest_access_token_subscribe" \
+        --header "Content-Type: application/json" \
+        --data-raw '{    
+        "validityTime": "3600",
+        "keyType": "PRODUCTION",
+        "accessAllowDomains": ["ALL"]
+        }'`
 
-    else
-    application_access_response=`curl -s --location -g --request POST "https://gateway.api.cloud.wso2.com/api/am/store/applications/generate-keys?applicationId=$application_id" \
-    --header "Authorization: Bearer $rest_access_token_subscribe" \
-    --header "Content-Type: application/json" \
-    --data-raw '{    
-    "validityTime": "3600",
-    "keyType": "PRODUCTION",
-    "accessAllowDomains": ["ALL"]
-    }'`
+        # echo $application_access_response
+        consumer_key=`echo "$application_access_response" | jq --raw-output '.consumerKey'`
+        consumer_secret=`echo "$application_access_response" | jq --raw-output '.consumerSecret'`
+    fi 
 
-    consumer_key=`echo "$application_access_response" | jq --raw-output '.consumerKey'`
-    consumer_secret=`echo "$application_access_response" | jq --raw-output '.consumerSecret'`
-    # echo $application_access_response
-fi 
-    echo $consumer_key
-    echo $consumer_secret
+        echo $consumer_key
+        echo $consumer_secret
 echo "::end-group"
 
 
 
 echo "::group::Generate access token for your API"
-# curl -u <client id>:<client secret> -k -d "grant_type=client_credentials&validity_period=3600" -H "Content-Type:application/x-www-form-urlencoded" https://gateway.api.cloud.wso2.com:443/token
-# base64 encode<consumer-key:consumer-secret>
-base64key3=`echo -n "$consumer_key:$consumer_secret" | base64`
+    # curl -u <client id>:<client secret> -k -d "grant_type=client_credentials&validity_period=3600" -H "Content-Type:application/x-www-form-urlencoded" https://gateway.api.cloud.wso2.com:443/token
+    # base64 encode<consumer-key:consumer-secret>
+    base64key3=`echo -n "$consumer_key:$consumer_secret" | base64`
 
-api_access_response=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com:443/token' \
---header "Content-Type: application/x-www-form-urlencoded" \
---header "Authorization: Basic $base64key3" \
---data-urlencode "grant_type=client_credentials" \
---data-urlencode "validity_period=3600"`
+    api_access_response=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com:443/token' \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    --header "Authorization: Basic $base64key3" \
+    --data-urlencode "grant_type=client_credentials" \
+    --data-urlencode "validity_period=3600"`
 
-api_access_token=`echo "$api_access_response" | jq --raw-output '.access_token'`
-# echo $api_access_response
-echo $api_access_token
+    api_access_token=`echo "$api_access_response" | jq --raw-output '.access_token'`
+   
+    # echo $api_access_response
+    echo $api_access_token
 echo "::end-group"
 
 
@@ -277,74 +279,6 @@ echo "::group::Create a file with important records"
 
 echo "::end-group"
 
-#-------------------------------
-
-
-echo "::group::Testing With Postman Collection"
-# newman run $5 --insecure
-echo "::end-group"
-
-
+#--------------------------------------------------------------------------------------
 apimcli logout wso2apicloud 
-
-
-# echo "::group::Export API from current Tenant"
-# # apimcli export-api -n <API-name> -v <version> -r <provider> -e <environment> -u <username> -p <password> -k
-# # apimcli export-api --name <API-name> --version <version> --provider <provider> --environment <environment> --username <username> --password <password> --insecure
-# # apimcli export-api -n TeamMasterAPI -v v1.0.0 -r mihindu@wso2.com@development -e wso2apicloud -k
-# echo "::end-group"
-
-# apimcli login wso2apicloud -u $3 -p $4 -k
-# echo "::group::Import API to Prod Tenant"
-# # apimcli import-api -f <environment>/<file> -e <environment> -u <username> -p <password> --preserve-provider <preserve_provider> -k
-# # apimcli import-api --file <environment>/<file> --environment <environment> --username <username> --password <password> --preserve-provider <preserve_provider> --insecure
-# # apimcli import-api -f wso2apicloud/TeamMasterAPI_v1.0.0.zip -e wso2apicloud --preserve-provider=false -k
-# apimcli import-api -f wso2apicloud/$5_$8.zip -e wso2apicloud --preserve-provider=false --update --verbose -k
-# # apimcli logout wso2apicloud 
-# echo "::end-group"
-
-# echo "::group::List APIS in a Prod Tenant"
-# # apimcli list apis -e <environment> -k
-# # apimcli list apis --environment <environment> --insecure
-# apimcli list apis -e wso2apicloud -k
-# echo "::end-group"
-# apimcli logout wso2apicloud 
-
-# echo "::group::Export an *App from current tenant"
-# # apimcli export-app -n <application-name> -o <owner> -e <environment> -k
-# # apimcli export-app --name <application-name> --owner <owner> --environment <environment> --insecure
-# # apimcli export-app -n TestApp -o kim@wso2.com@testorg1 -e wso2apicloud -k
-# echo "::end-group"
-
-# echo "::group::Export an *App from current tenant"
-# # apimcli export-app -n <application-name> -o <owner> -e <environment> -k
-# # apimcli export-app --name <application-name> --owner <owner> --environment <environment> --insecure
-# # apimcli export-app -n TestApp -o kim@wso2.com@testorg1 -e wso2apicloud -k
-# echo "::end-group"
-
-# echo "::group::List *Apps in a perticular environment"
-# # apimcli list apps -e <environment> -o <owner> -k
-# # apimcli list apps --environment <environment> --owner <owner> --insecure
-# # apimcli list apps -e wso2apicloud -o admin -k
-# echo "::end-group"
-
-# echo "::group:: Reset Users"
-# # If you want to list APIs or applications that belong to a particular tenant, 
-# # you need to first reset the user before listing APIs or applications for the particular tenant.
-
-# # apimcli reset-user -e <environment>
-# # apimcli reset-user --environment <environment>
-# # apimcli reset-user -e wso2apicloud
-# echo "::end-group"
-
-# echo "::group:: Set HTTP request timeout "
-# # apimcli set --http-request-timeout <http-request-timeout>
-# # apimcli set --http-request-timeout 10000
-# echo "::end-group"
-
-# echo "::group:: Set export directory "
-# #Run the following CLI command to the change the default location of the export directory.
-# # apimcli set --export-directory <export-directory-path>
-# # apimcli set --export-directory /Users/kim/Downloads/MyExports
-# echo "::end-group"
 
