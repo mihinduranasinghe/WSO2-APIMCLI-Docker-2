ce2cc38a13dab4b79233d864c9c64585c1c9cd61
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-29 00:52:19 +0530

    refactoring 2

--- .github/actions/wso2apim/script.sh
+++ .github/actions/wso2apim/script.sh
@@ -69,100 +69,93 @@ echo "::group::Testing With Postman Collection"
 echo "::end-group"
 
 
-
 echo "::group::Import API project to targetted Tenant"
-apimcli login wso2apicloud -u $1 -p $2 -k
-apimcli import-api -f ./$3/$4 -e wso2apicloud --preserve-provider=false --update --verbose -k
-# apimcli logout wso2apicloud 
+    apimcli login wso2apicloud -u $1 -p $2 -k
+    apimcli import-api -f ./$3/$4 -e wso2apicloud --preserve-provider=false --update --verbose -k
 echo "::end-group"
-
 
 
 echo "::group::List APIS in targetted Tenant"
-# apimcli list apis -e <environment> -k
-# apimcli list apis --environment <environment> --insec# echo "::set-env name=HELLO::hello"ure
-apimcli list apis -e wso2apicloud -k
+    # apimcli list apis -e <environment> -k
+    # apimcli list apis --environment <environment> --insec# echo "::set-env name=HELLO::hello"ure
+    apimcli list apis -e wso2apicloud -k
 echo "::end-group"
 
 
-#-------------------------------
-# Invoking and testing automation
+#-------------------------------Invoking and testing automation
 
 echo "::group::Client Registration"
-# curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
-# base64key1 = Authorization: Basic  <username@wso2.com@organizationname:password>base64
-base64key1=`echo -n "$1:$2" | base64`
+    # curl -X POST -H "Authorization: Basic base64encode(<email_username@Org_key>:<password>)" -H "Content-Type: application/json" -d @payload.json https://gateway.api.cloud.wso2.com/client-registration/register
+    # base64key1 = Authorization: Basic  <username@wso2.com@organizationname:password>base64
+    base64key1=`echo -n "$1:$2" | base64`
 
-rest_client_object=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/client-registration/register' \
---header "Authorization: Basic $base64key1" \
---header "Content-Type: application/json" \
---data-raw '{
-    "callbackUrl": "www.google.lk",
-    "clientName": "rest_api_store",
-    "tokenScope": "Production",
-    "owner": "'$1'",
-    "grantType": "password refresh_token",
-    "saasApp": true
-}'`
+    rest_client_object=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/client-registration/register' \
+    --header "Authorization: Basic $base64key1" \
+    --header "Content-Type: application/json" \
+    --data-raw '{
+        "callbackUrl": "www.google.lk",
+        "clientName": "rest_api_store",
+        "tokenScope": "Production",
+        "owner": "'$1'",
+        "grantType": "password refresh_token",
+        "saasApp": true
+    }'`
 
-rest_clientId=`echo "$rest_client_object" | jq --raw-output '.clientId'`
-rest_clientSecret=`echo "$rest_client_object" | jq --raw-output '.clientSecret'`
+    rest_clientId=`echo "$rest_client_object" | jq --raw-output '.clientId'`
+    rest_clientSecret=`echo "$rest_client_object" | jq --raw-output '.clientSecret'`
 
-echo $rest_clientId
-echo $rest_clientSecret 
+    echo $rest_clientId
+    echo $rest_clientSecret 
 echo "::end-group"
-
 
 
 echo "::group::Client Access Token Generate"
-# curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
-# base64key2 = Authorization: Basic <rest-client-id:rest-client-secret>base64
-base64key2=`echo -n "$rest_clientId:$rest_clientSecret" | base64`
+    # curl -k -d "grant_type=password&username=email_username@Org_key&password=admin&scope=apim:subscribe" -H "Authorization: Basic SGZFbDFqSlBkZzV0YnRyeGhBd3liTjA1UUdvYTpsNmMwYW9MY1dSM2Z3ZXpIaGM3WG9HT2h0NUFh" https://gateway.api.cloud.wso2.com/token
+    # base64key2 = Authorization: Basic <rest-client-id:rest-client-secret>base64
+    base64key2=`echo -n "$rest_clientId:$rest_clientSecret" | base64`
 
-rest_access_token_scope_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
---header "Content-Type: application/x-www-form-urlencoded" \
---header "Authorization: Basic $base64key2" \
---data-urlencode "grant_type=password" \
---data-urlencode "username=$1" \
---data-urlencode "password=$2" \
---data-urlencode "scope=apim:api_view" | jq --raw-output '.access_token'`
+    rest_access_token_scope_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    --header "Authorization: Basic $base64key2" \
+    --data-urlencode "grant_type=password" \
+    --data-urlencode "username=$1" \
+    --data-urlencode "password=$2" \
+    --data-urlencode "scope=apim:api_view" | jq --raw-output '.access_token'`
 
-rest_access_token_subscribe=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
---header "Content-Type: application/x-www-form-urlencoded" \
---header "Authorization: Basic $base64key2" \
---data-urlencode "grant_type=password" \
---data-urlencode "username=$1" \
---data-urlencode "password=$2" \
---data-urlencode "scope=apim:subscribe" | jq --raw-output '.access_token'`
+    rest_access_token_subscribe=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    --header "Authorization: Basic $base64key2" \
+    --data-urlencode "grant_type=password" \
+    --data-urlencode "username=$1" \
+    --data-urlencode "password=$2" \
+    --data-urlencode "scope=apim:subscribe" | jq --raw-output '.access_token'`
 
-rest_access_token_subscription_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
---header "Content-Type: application/x-www-form-urlencoded" \
---header "Authorization: Basic $base64key2" \
---data-urlencode "grant_type=password" \
---data-urlencode "username=$1" \
---data-urlencode "password=$2" \
---data-urlencode "scope=apim:subscription_view" | jq --raw-output '.access_token'`
+    rest_access_token_subscription_view=`curl -s --location -g --request POST 'https://gateway.api.cloud.wso2.com/token' \
+    --header "Content-Type: application/x-www-form-urlencoded" \
+    --header "Authorization: Basic $base64key2" \
+    --data-urlencode "grant_type=password" \
+    --data-urlencode "username=$1" \
+    --data-urlencode "password=$2" \
+    --data-urlencode "scope=apim:subscription_view" | jq --raw-output '.access_token'`
 
-echo $rest_access_token_scope_view
-echo $rest_access_token_subscribe
-echo $rest_access_token_subscription_view
+    echo $rest_access_token_scope_view
+    echo $rest_access_token_subscribe
+    echo $rest_access_token_subscription_view
 echo "::end-group"
-
 
 
 echo "::group::Finding the API identifier"
-API_response=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/publisher/apis' \
---header "Authorization: Bearer $rest_access_token_scope_view"`
+    API_response=`curl -s --location -g --request GET 'https://gateway.api.cloud.wso2.com/api/am/publisher/apis' \
+    --header "Authorization: Bearer $rest_access_token_scope_view"`
 
-all_APIs_list=`echo "$API_response" | jq '.list'`
-relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$3'" and .version=="'$4'")'`
-api_identifier=`echo "$relevant_api" | jq --raw-output '.id'`
-echo $API_response
-# echo $all_APIs_list
-# echo $relevant_api
-echo $api_identifier
+    all_APIs_list=`echo "$API_response" | jq '.list'`
+    relevant_api=`echo "$all_APIs_list" | jq '.[] | select(.name=="'$3'" and .version=="'$4'")'`
+    api_identifier=`echo "$relevant_api" | jq --raw-output '.id'`
+    echo $API_response
+    # echo $all_APIs_list
+    # echo $relevant_api
+    echo $api_identifier
 echo "::end-group"
-
 
 
 echo "::group::Create new Application"
